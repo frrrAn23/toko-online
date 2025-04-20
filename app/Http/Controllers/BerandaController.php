1841b0798afeb8070ea597aa079620bb5e238fc5
frrrAn23 <ferrrkhoirulanam666@gmail.com>
2025-04-20 11:49:12 +0700

    pertemuan 4 solve .env web.php

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Produk;
 
@@ -17,7 +17,12 @@ class BerandaController extends Controller
     }
     public function index()
     {
+        // dd(session()->all()); // <- tambahin di sini
+
         $produk = Produk::where('status', 1)->orderBy('updated_at', 'desc')->paginate(6);
+
+        // dd(Auth::check(), Auth::user());
+
         return view('v_beranda.index', [
             'judul' => 'Halan Beranda',
             'produk' => $produk,
--- routes/web.php
+++ routes/web.php
@@ -50,15 +50,19 @@ Route::post('backend/laporan/cetakuser', [UserController::class, 'cetakUser'])->
 Route::get('backend/laporan/formproduk', [ProdukController::class, 'formProduk'])->name('backend.laporan.formproduk')->middleware('auth');
 Route::post('backend/laporan/cetakproduk', [ProdukController::class, 'cetakProduk'])->name('backend.laporan.cetakproduk')->middleware('auth');
 
-Route::get('/produk/detail/{id}', [ProdukController::class, 'detail'])->name('produk.detail');
-Route::get('/produk/kategori/{id}', [ProdukController::class, 'produkKategori'])->name('produk.kategori');
-Route::get('/produk/all', [ProdukController::class, 'produkAll'])->name('produk.all');
+Route::middleware(['web'])->group(function () {
+    // Frontend 
+    Route::get('/beranda', [BerandaController::class, 'index'])->name('beranda'); 
+    Route::get('/produk/detail/{id}', [ProdukController::class, 'detail'])->name('produk.detail');
+    Route::get('/produk/kategori/{id}', [ProdukController::class, 'produkKategori'])->name('produk.kategori');
+    Route::get('/produk/all', [ProdukController::class, 'produkAll'])->name('produk.all');
+});
 
 //API Google
 Route::get('/auth/redirect', [CustomerController::class, 'redirect'])->name('auth.redirect');
 Route::get('/auth/google/callback', [CustomerController::class, 'callback'])->name('auth.callback');
 // Logout
-Route::post('/logout', [CustomerController::class, 'logout'])->name('logout');
+Route::post('/logout', [CustomerController::class, 'logout'])->name('customer.logout');
 
 // Route untuk Customer
 Route::resource('backend/customer', CustomerController::class, ['as' => 'backend'])->middleware('auth');
